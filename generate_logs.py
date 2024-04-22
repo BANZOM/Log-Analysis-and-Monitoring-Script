d8c753cc693e9bb0be5805d65b66664a2b924a02
banzom <adityap@cabin4j.com>
2024-04-22 16:38:06 +0530

    Refactor GenerateLogs class to support generating a specified number of logs in generate_logs.py

--- generate_logs.py
+++ generate_logs.py
@@ -10,6 +10,7 @@ class GenerateLogs:
         levels: A list of log levels.
         messages: A list of log messages.
         log_file: A string representing the log file path.
+        num_logs: An integer representing the number of logs to generate. if 0, it will generate logs indefinitely.
 
     Methods:
         generate_logs: Generates logs based on specified parameters.
@@ -39,13 +40,22 @@ class GenerateLogs:
         """
         Generates logs based on specified parameters.
         """
-        while True:
-            level = random.choice(self.levels)
-            message = random.choice(self.messages)
-            log_entry = self.generate_log_entry(level, message)
-            with open(self.log_file, 'a') as f:
-                f.write(log_entry)
-            time.sleep(random.uniform(1, 5))
+        if self.num_logs == 0:
+            while True:
+                level = random.choice(self.levels)
+                message = random.choice(self.messages)
+                log_entry = self.generate_log_entry(level, message)
+                with open(self.log_file, 'a') as f:
+                    f.write(log_entry)
+                time.sleep(random.uniform(1, 5))
+        else:
+            for _ in range(self.num_logs):
+                level = random.choice(self.levels)
+                message = random.choice(self.messages)
+                log_entry = self.generate_log_entry(level, message)
+                with open(self.log_file, 'a') as f:
+                    f.write(log_entry)
+                time.sleep(random.uniform(1, 5))
 
     def generate_log_entry(self, level: str, message: str) -> str:
         """
